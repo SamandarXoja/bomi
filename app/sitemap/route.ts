e9ea3d97e105a1594ad0c357246de53761bf462c
User <samandarsaidahmadov98@gmail.com>
2025-02-20 14:00:41 +0500

    s

---
+++ app/sitemap/route.ts
@@ -0,0 +1,14 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+    return [
+        {
+            url: "https://bomi-test.vercel.app/en",
+            lastModified: new Date(),
+        },
+        {
+            url: "https://bomi-test.vercel.app/en",
+            lastModified: new Date(),
+        },
+    ];
+}
